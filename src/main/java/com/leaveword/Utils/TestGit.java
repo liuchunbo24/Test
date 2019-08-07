63de3ef0880bad151be34758ee9105dbcbf7fe07
刘春波 <980168687@qq.com>
2019-08-07 15:14:07 +0800

    2019-08-07 two

--- src/main/java/com/leaveword/Utils/TestGit.java
+++ src/main/java/com/leaveword/Utils/TestGit.java
@@ -1,10 +1,13 @@
 package com.leaveword.Utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Created by Administrator on 2019/8/7.
  */
+@Slf4j
 public class TestGit {
     public static void main(){
-        
+        log.info("这是一次测试。。");
     }
 }
